2536a43fd0d675f0f80125a70f4753c5e5609d1b
valemonteverde17 <valeria.banuelosmonteverde@umg.edu.mx>
2024-04-22 21:32:47 -0600

    Tercer entregable 22-04-2024

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./views/About";
 import SignIn from "./views/SignIn";
 import SignUp from "./views/SignUp";
 import Page404 from "./views/others/Page404";
+import Page500 from "./views/others/Page500";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Route path="/signin" element = {<SignIn />} />
       <Route path="/signup" element = {<SignUp />} />
       <Route path="/*" element = {<Page404 />} />
+      <Route path="/*" element = {<Page500 />} />
     </Routes>
   </>
   );
