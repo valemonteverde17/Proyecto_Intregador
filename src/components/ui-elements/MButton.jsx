967bd586656019902466767b2e25f237e5d6ef5c
Mario <marioarturo.gaytanorozco@umg.edu.mx.com>
2024-04-30 23:35:57 -0600

    30/04/2024 4to entregable ui Rosario y Mario

--- src/components/ui-elements/MButton.jsx
+++ src/components/ui-elements/MButton.jsx
@@ -1,22 +1,24 @@
 import { Button } from "@chakra-ui/react";
 import React from "react";
 
-function MButton({ children, variant }) {
+function MButton({children, variant, onClick, ...props }) {
   let background;
   switch (variant) {
     case "general":
       background = "#FF3F00AA";
       break;
     case "aceptar":
+      background = "green";
       break;
     case "cancelar":
+      background = "red";
       break;
     default:
       background = "#FF3F00AA";
       break;
   }
   return (
-    <Button borderRadius={30} bgColor={background}>
+    <Button borderRadius={30} bgColor={background} onClick={onClick} {...props}>
       {children}
     </Button>
   );
