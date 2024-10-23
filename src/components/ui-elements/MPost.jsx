688c8bbdd8104ab2e88972b825e19ad1069ce9a2
rosasrosario <rosario.rosasperez@umg.edu.mx>
2024-10-23 16:10:42 -0600

    mpost modificado by RR

--- src/components/ui-elements/MPost.jsx
+++ src/components/ui-elements/MPost.jsx
@@ -1,21 +1,18 @@
 import React from "react";
-import { Box, Button, Badge, IconButton, Avatar } from "@chakra-ui/react";
+import { Box, Text, Button } from "@chakra-ui/react";
 import { StarIcon } from "@chakra-ui/icons";
 
 function MPost({
   children,
-  username,
-  category,
-  privacy,
+  username = "Usuario",
+  category = "General",
+  privacy = "Público",
   avatar,
-  likes = 0,
+  likes = 40,
   ...props
 }) {
-  const userImage =
-    "https://img.freepik.com/fotos-premium/cara-cara-mapache-cara-linda-animal-tranquilo-pacifico-hermoso-retrato_21085-4272.jpg";
-
   const buttonProps = {
-    variant: "outline",
+    variant: "ghost",
     colorScheme: "orange",
     size: "sm",
     ...props,
@@ -24,49 +21,50 @@ function MPost({
   return (
     <Box
       position="relative"
-      border="5px solid #D4E1FF"
-      borderRadius="md"
-      padding="2"
-      marginBottom="5"
-      backgroundColor="#D4E1FF"
-      marginTop="50px"
+      borderRadius="lg"
+      backgroundColor="#C7C8FB"
+      padding="15px"
+      marginTop="50px" 
+      maxWidth="550px" 
+     
     >
+      <Text fontSize="lg" fontWeight="bold">
+        {children}
+      </Text>
+
+      
       <Box
         position="absolute"
-        top="-45px"
-        left="5.8%"
-        transform="translateX(-50%)"
-        backgroundColor="#A4D9FF"
-        borderRadius="md"
-        padding="2"
+        top="-10%"
+        left="0"
+        transform="translateY(-40%)"
+        backgroundColor="#FF8B60"
+        borderRadius="lg"
+        padding="4px 10px"
+        display="flex"
+        alignItems="flex-start"
+        zIndex="-1"
       >
-        <Badge colorScheme="black">{username}</Badge>
+        <Text fontSize="sm" color="white" fontWeight="bold">
+          @{username}
+        </Text>
       </Box>
-      <Box display="flex" alignItems="center" marginBottom="2">
-        <Box marginRight="2">
-          <Avatar size="lg" src={avatar && avatar} />
-        </Box>
-        <Box flex="1">
-          <Box fontSize="xl" fontWeight="bold">
-            {username}
-          </Box>
-          <Box fontSize="md">{children}</Box>
-          <Button {...buttonProps}>Privacidad: {privacy}</Button>
-          <Button {...buttonProps}>Categoría: {category}</Button>
-          <Button {...buttonProps} leftIcon={<StarIcon />}>
-            Likes: {likes}
-          </Button>
-        </Box>
-      </Box>
+
       <Box
         display="flex"
-        justifyContent="space-between"
+        justifyContent="flex-end"
         alignItems="center"
-        marginTop="2"
+        marginTop="50px"
       >
-        <Box>
-          <Button {...buttonProps}>Comentar</Button>
+        <Box display="flex" alignItems="center" color="orange.400" marginRight="10px">
+          <Text fontSize="xs" marginRight="4px">
+            {likes}
+          </Text>
+          <StarIcon boxSize={5} />
         </Box>
+        <Button size="sm" colorScheme="orange" variant="ghost">
+          Comentarios
+        </Button>
       </Box>
     </Box>
   );
